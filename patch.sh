4359fa8522298c2c6902f4fd3a3cc5576f4887b6
wolfyu1991 <wolfyu1991@qq.com>
2020-04-20 09:00:53 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -17,10 +17,11 @@ cp -f ../mt7621_xiaoyu_xy-c3.dts target/linux/ramips/dts/
 sed -i 's/xy-c5/xy-c3/g' target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 
 # Modify the version number
-sed -i "s/OpenWrt /wolfyu build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+# sed -i "s/OpenWrt /wolfyu build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /wolfyu build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
@@ -39,6 +40,8 @@ cp -f ../banner package/base-files/files/etc/
 # add ssr plus+
 git clone https://github.com/fw876/helloworld.git package/helloworld
 # add OpenAppFilter
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # add serverchan
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+# add luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon
